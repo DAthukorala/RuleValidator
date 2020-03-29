0b57c8e610920955f3c76d15312dd5851be29da9
DhanushkaA <dannya@gbs.kiwi>
2020-03-30 12:51:32 +1300

    added editable div and value by type

--- scripts/validations.js
+++ scripts/validations.js
@@ -12,30 +12,30 @@ $(function () {
 
         required_text_when_checks.each(function (index, element) {
             var rules = $(element).attr("required_text_when_check");
-            rules = rules.split('_');
+            rules = rules.split(':');
 
             //must have only one checkBox (lhs) and many textboxes(rhs)
-            if (rules.length == 2) {
+            if (rules.length === 2) {
                 var checkBoxId = rules[0];
                 var textBoxIds = rules[1].split(',');
                 var isChecked = $("#" + checkBoxId).is(":checked");
 
-                textBoxIds.forEach(function (txtId) {
-                    var value = $("#" + txtId).val().trim();
+                textBoxIds.forEach(function (id) {
+                    var value = getValueByType(id);
 
                     if (isChecked) {
                         if (value) {
-                            $("#" + txtId).removeClass("invalidBorder");
-                            valids.push(txtId);
+                            $("#" + id).removeClass("invalidBorder");
+                            valids.push(id);
                         }
                         else {
-                            $('#' + txtId).addClass('invalidBorder');
+                            $('#' + id).addClass('invalidBorder');
                             invalids.push();
                         }
                     }
                     else {
-                        $("#" + txtId).removeClass("invalidBorder");
-                        valids.push(txtId);
+                        $("#" + id).removeClass("invalidBorder");
+                        valids.push(id);
                     }
                 });
             }
@@ -43,6 +43,30 @@ $(function () {
 
         //alternatively return validation summary for controls
         return { Valids: valids, Invalids: invalids };
+    }
+
+    function getValueByType(id) {
+        var result = "";
+        var type = getType(id);
+        if (type === "div") {
+            result = $("#" + id + "").text();
+        }
+        else if (type === "text" || type === "textarea") {
+            result = $("#" + id + "").val();
+        }
+        return result.trim();
+    }
+
+    function getType(id) {
+        var control = $("#" + id + "").get(0);
+        var type = "";
+        if (control.tagName === "DIV") {
+            type = control.tagName.toLowerCase();
+        }
+        else {
+            type = control.type;
+        }
+        return type;
     }
 
 });
